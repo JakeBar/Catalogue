5e13c83e5d53208c868eac470ce73018bd3bd30d
JakeBar <ortin666@hotmail.com>
2015-02-02 17:16:07 +1100

    Corrected product dates
    -Stored data in a list and added a creation method

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 date = Date.today + 5.days
- 
-products = Product.create([
-  { name: "Milk", volume: "2L", quantity: 10, price: 3.95, sell_by: date + 5.days },
-  { name: "Juice", volume: "2L", quantity: 25, price: 6.5, sell_by: date + 2.weeks },
-  { name: "Water", volume: "750mL", quantity: 50, price: 3.99, sell_by: date + 6.months },
-  { name: "Yoghurt", volume: "200mL", quantity: 12, price: 2.5, sell_by: date + 5.days },
-  { name: "Cola", volume: "375mL", quantity: 100, price: 2.95, sell_by: date + 3.months },
-  { name: "Soda Water", volume: "750mL", quantity: 75, price: 1.99, sell_by: date + 6.months }
-])
- 
+
+products = [
+[ "Milk", "2L", 10, 3.95, date + 5.days],
+[ "Juice", "2L", 25, 6.5, date + 2.weeks],
+[ "Water", "750mL", 50, 3.99, date + 6.months],
+[ "Yoghurt", "200mL", 12, 2.5, date + 5.days],
+[ "Cola", "375mL", 100, 2.95, date + 3.months],
+[ "Soda Water", "750mL", 75, 1.99, date + 6.months]
+]
+
+
 # The manufacturer is in a rush and has determined that all products should expire 5 days after the date they must be sold.
- 
-#products.each do | product |
-   
- # product.best_before = product.sell_by + 5.days
- # products.save!
- 
-#end
+
+
+products.each do | product |
+    Product.create( :name => product[0], :volume => product[1],
+                   :quantity => product[2],  :price  => product[3], :sell_by => product[4] + 5)
+end
